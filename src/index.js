ec88013a8e6d4bd6972c38d32eb1a1f7565e6fd1
Jason Ni <jasonni@ucsb.edu>
2022-07-17 16:25:40 -0400

    router basename

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
   <Provider store={store}>
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
       <Routes>
           <Route exact path="/" element={<App />} /> 
           <Route
