f21f9279c53049715612ed0ff63097201c86bf29
Jason Ni <jasonni@ucsb.edu>
2022-08-25 17:11:27 -0700

    redesign

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
